8236385b135e908718aa16069d1c2d723e0d0a25
adelen99 <lupuadelin1999@yahoo.com>
2025-01-31 09:04:12 +0200

    titlu updated

--- app/page.tsx
+++ app/page.tsx
@@ -10,9 +10,7 @@ export default function Home() {
       {/* Secțiune pentru descriere și imagine */}
       <div className="grid md:flex items-center justify-center gap-12 w-11/12 md:w-3/4 mx-auto my-0 sm:my-16 p-4">
         <div className="text-center md:text-left md:w-1/2 mt-0 sm:mt-6 md:mt-0">
-          <h2 className="text-3xl font-bold text-orange-400">
-            Descoperă Mâncărurile Noastre
-          </h2>
+          <h2 className="text-3xl font-bold text-orange-400">Răsfăț Culinar</h2>
           <p className="mt-4 text-lg text-gray-300">
             Explorează o selecție rafinată de preparate realizate cu ingrediente
             proaspete și pasiune pentru gust. Fiecare farfurie este o experiență
