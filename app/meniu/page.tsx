0cef9288fb0f0a8dc311f452136c13941ecbb041
mgtecindustry <administrator@mgtecindustry.ro>
2025-02-07 11:15:43 +0200

    Meniu telefon

--- app/meniu/page.tsx
+++ app/meniu/page.tsx
@@ -1,36 +1,27 @@
-"use client";
-import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
-const MenuPage = () => {
-  const [isMobile, setIsMobile] = useState(false);
-  const meniuUrl =
-    "https://oqjgdpuknnexaacahemq.supabase.co/storage/v1/object/sign/TerasaBlackTulip/meniu.pdf?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJUZXJhc2FCbGFja1R1bGlwL21lbml1LnBkZiIsImlhdCI6MTczODkxNDAyNiwiZXhwIjoxODk2NTk0MDI2fQ.AvzALbKRhZzeBAnxMd9ouRWNb4DY9zDwpAIQXZyiD60";
-
-  useEffect(() => {
-    const checkIfMobile = () => {
-      setIsMobile(window.innerWidth < 640);
-    };
-
-    checkIfMobile();
-    window.addEventListener("resize", checkIfMobile);
-
-    return () => window.removeEventListener("resize", checkIfMobile);
-  }, []);
+export default function Menu() {
+  const pages = Array.from(
+    { length: 22 },
+    (_, i) => `/meniu/meniu${i + 1}.jpg`
+  );
 
   return (
-    <div className="w-full h-screen">
-      <iframe
-        src={`${meniuUrl}#view=FitW&zoom=page-fit${isMobile ? "&page=1" : ""}`}
-        className="w-full h-full border-none"
-        style={{
-          overflow: "auto",
-          WebkitOverflowScrolling: "touch",
-        }}
-        title="Meniu"
-        allow="fullscreen"
-      />
+    <div>
+      <div className="menu-images grid grid-cols-1 gap-4">
+        {pages.map((page, index) => (
+          <div key={index} className="menu-image ">
+            <Image
+              src={page}
+              alt={`Pagina ${index + 1}`}
+              layout="responsive"
+              width={600}
+              height={800}
+              className="object-cover w-full h-full"
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
-};
-
-export default MenuPage;
+}
